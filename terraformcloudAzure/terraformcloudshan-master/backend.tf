b9d76da27ef7d4844c660e363f595e48063e3206
RupinderjitGhuman <53123141+RupinderjitGhuman@users.noreply.github.com>
2022-02-10 11:21:42 -0800

    Update backend.tf

--- terraformcloudAzure/terraformcloudshan-master/backend.tf
+++ terraformcloudAzure/terraformcloudshan-master/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "testResourceGroup1"
-    storage_account_name = "storageaccountnamedemo12"
+    resource_group_name  = "TerraForm-RG-01"
+    storage_account_name = "storageaccountnameterraform01"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
   }
